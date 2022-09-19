33cac374d85250a84e0ecb2249449da15abd4550
andre.borba <dev_andre.filipe@outlook.com>
2022-09-18 23:57:34 -0300

    changes

--- src/sintax/pointer.cpp
+++ src/sintax/pointer.cpp
@@ -5,8 +5,9 @@ int main()
   int n = 2;
   int *pn = &n;
 
-  std::cout << "pn = " << pn << '\n';
-  std::cout << "*pn = " << *pn << '\n';
+  std::cout << "pn = " << pn << '\n'; // 0x7fff5fbff7f8
+  std::cout << "*pn = " << *pn << '\n'; // 2
+  std::cout << "&n = " << &n << '\n'; // 0x7fff5fbff7f8
 
   return 0;
 }
